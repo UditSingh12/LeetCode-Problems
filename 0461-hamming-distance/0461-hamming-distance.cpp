8f4d8b850a3eff9c2103e16c61879db9f3831242
Udit Pratap Singh <120353277+UditSingh12@users.noreply.github.com>
2026-07-14 16:52:58 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.97%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int diff = x ^ y;
+        int count = 0;
+
+        while (diff) {
+            count += diff & 1;
+            diff >>= 1;
+        }
+
+        return count++;
+    }
+};
