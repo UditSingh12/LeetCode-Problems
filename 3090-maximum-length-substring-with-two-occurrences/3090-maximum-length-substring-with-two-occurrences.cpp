e10198e6d91b3fd36866a240aac8efb94754b3e9
Udit Pratap Singh <120353277+UditSingh12@users.noreply.github.com>
2026-08-14 20:46:00 +0530

    Time: 3 ms (42.93%), Space: 11.8 MB (18.49%) - LeetHub

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        int n = s.length();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+
+                // More than 2 occurrences 
+                if (freq[s[j] - 'a'] > 2)
+                    break;
+
+                ans = max(ans, j - i + 1);
+            }
+        }
+
+        return ans;
+    }
+};
