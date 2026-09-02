39a3b1c2cc206c4fd3c9468e5a1abb75955593b2
Udit Pratap Singh <120353277+UditSingh12@users.noreply.github.com>
2026-09-02 09:35:27 +0530

    Time: 0 ms (100%), Space: 30.5 MB (7.23%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -2,6 +2,13 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         bool hasOdd = false;
+
+        for (int x : nums1) {
+            if (x % 2 != 0) {
+                hasOdd = true;
+                break;
+            }
+        }
         return true;
     }
 };
