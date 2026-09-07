8ecca367e79bc0b96027b89d1bfc8524f978002c
Udit Pratap Singh <120353277+UditSingh12@users.noreply.github.com>
2026-09-08 01:56:15 +0530

    Time: 0 ms (100%), Space: 8.8 MB (91.16%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const long long MOD = 1e9 + 7;
+
+        long long dp = 1;  // empty subsequence
+
+        vector<long long> last(26, 0);
+
+        for (char ch : s) {
+            int c = ch - 'a';
+
+            long long newDp = (2 * dp - last[c] + MOD) % MOD;
+
+            last[c] = dp;
+            dp = newDp;
+        }
+
+        // Remove empty subsequence
+        return (dp - 1 + MOD) % MOD;
+    }
+};
